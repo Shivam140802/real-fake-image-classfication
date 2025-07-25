bf634c0cf3389ee2dc5b41616852b1e5120e1ddd
Shivam Gupta <sg4781778@gmail.com>
2025-07-25 11:38:41 +0530

    add all

---
+++ README.md
@@ -0,0 +1,5 @@
+CNN-Based Image Classification (Real vs. Fake Images)
+• Designed and trained 6 CNN models using TensorFlow and Keras to classify 140K images from Kaggle (70K
+real, 70K fake).
+• Achieved 94% training accuracy and 88% testing accuracy through hyperparameter tuning and regularization.
+• Applied dropout, batch normalization, and model variation to enhance performance and reduce overfitting
---
+++ hugging.py
@@ -0,0 +1,18 @@
+from huggingface_hub import upload_file
+
+repo_id = "shivammmmmmm/image-classification-models"
+
+for i in range(1, 7):
+    model_filename = f"best_model{i}.keras"
+    local_path = f"models/{model_filename}"
+    repo_path = f"model_{i}.keras"
+
+    print(f"Uploading: {local_path} → {repo_path}")
+    upload_file(
+        path_or_fileobj=local_path,
+        path_in_repo=repo_path,
+        repo_id=repo_id,
+        repo_type="model"
+    )
+
+print("All models uploaded successfully.")
